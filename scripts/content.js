0ba3de089dee7b8d7a00374da897a877b2cfb3d1
rajat4984 <rs20021023@gmail.com>
2025-02-02 10:58:57 +0530

    edit cover button added

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
-    
+  $(document).delegate(
+    ".ytmusic-menu-navigation-item-renderer",
+    "click",
+    function () {
+      if ($(this).text().trim() === "Edit playlist") {
+        setTimeout(() => {
+          // Check if input already exists
+          if ($(".privacy-dropdown").next(".add_cover_btn").length === 0) {
+            let inputFile = $("<input>", {
+              type: "file",
+              accept: "image/*",
+              class: "add_cover_btn",
+            });
+
+            inputFile.insertAfter(".privacy-dropdown");
+            console.log("Input added");
+          } else {
+            console.log("Input already exists");
+          }
+        }, 500);
+      }
+    }
+  );
 });
