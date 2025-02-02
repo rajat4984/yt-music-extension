54382df60d1ce0825ff50f0e0afe7bd2c56b36be
rajat4984 <rs20021023@gmail.com>
2025-02-02 12:08:47 +0530

    images getting stored in indexeddb

--- scripts/content.js
+++ scripts/content.js
@@ -3,9 +3,11 @@ $(document).ready(function () {
     ".ytmusic-menu-navigation-item-renderer",
     "click",
     function () {
+      // Check if the clicked element is the "Edit playlist" button
       if ($(this).text().trim() === "Edit playlist") {
         setTimeout(() => {
-          // Check if input already exists
+          let playListId = $(this).attr("href").split("/")[1];
+          console.log(playListId, "playlistid");
           if ($(".privacy-dropdown").next(".add_cover_btn").length === 0) {
             let inputFile = $("<input>", {
               type: "file",
@@ -14,12 +16,63 @@ $(document).ready(function () {
             });
 
             inputFile.insertAfter(".privacy-dropdown");
-            console.log("Input added");
-          } else {
-            console.log("Input already exists");
+
+            inputFile.on("change", function (event) {
+              let file = event.target.files[0];
+              if (file) {
+                let reader = new FileReader();
+                reader.onload = function (e) {
+                  let imageData = e.target.result;
+
+                  // Open IndexedDB
+                  let request = indexedDB.open("ytMusicDB", 1);
+
+                  request.onupgradeneeded = function (event) {
+                    let db = event.target.result;
+                    if (!db.objectStoreNames.contains("images")) {
+                      db.createObjectStore("images", {
+                        keyPath: "id",
+                      });
+                    }
+                  };
+
+                  request.onsuccess = function (event) {
+                    let db = event.target.result;
+                    let transaction = db.transaction("images", "readwrite");
+                    let store = transaction.objectStore("images");
+
+                    let imageRecord = {
+                      id: playListId,
+                      data: imageData,
+                      name: file.name,
+                      timestamp: new Date().toISOString(),
+                    };
+
+                    let addRequest = store.add(imageRecord);
+
+                    addRequest.onsuccess = function () {
+                      console.log("Image added to IndexedDB:", imageRecord);
+                    };
+
+                    addRequest.onerror = function () {
+                      console.error("Failed to add image to IndexedDB");
+                    };
+                  };
+
+                  request.onerror = function (event) {
+                    console.error("Error opening IndexedDB", event);
+                  };
+                };
+
+                reader.readAsDataURL(file);
+              }
+            });
           }
         }, 500);
       }
     }
   );
 });
+
+
+ 
